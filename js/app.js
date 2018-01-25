4cfed763e0dee62b781eed9005f8eeff9c6bfb98
sida <siningdream@gmail.com>
2018-01-25 21:05:35 +0800

    style: use atom beautify to format the codes

--- js/app.js
+++ js/app.js
@@ -79,13 +79,13 @@ var allEnemies = enemyRows.map(row => new Enemy(row));
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener('keyup', function (e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
